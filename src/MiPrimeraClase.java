3dd93341cb344fc61d822d8f74ee4786e9bf2a68
anec0 <anec0@10.111.165.26>
2021-02-04 16:53:03 +0100

    Version inicial

---
+++ src/MiPrimeraClase.java
@@ -0,0 +1,14 @@
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es un mensaje");
+		otroMetodo();
+	}
+	
+	private static void otroMetodo() {
+		System.out.println("Otro metodo");
+	}
+
+}
